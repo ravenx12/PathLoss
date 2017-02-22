a3d653e4aa87915e6ff6e6c624cd48f67121590e
ray <rayashman@btinternet.com>
2017-02-22 20:18:05 +0000

    Added power to db and db to power functions

--- resources/converter.js
+++ resources/converter.js
@@ -1,9 +1,17 @@
 
-
-
+//This function simply takes in power
+// and returns the decibel value
 function dbConvert(power)
 {
     dB = 10*Math.log10( power/1);
     return dB;
+}
+
+//This function  takes in decibel
+// and returns the power value
+function powerConvert(dB)
+{
+    power = math.power(10,D7/10);
+    return power;
 }
 
